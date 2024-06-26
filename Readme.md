e32a8c9801c6f77b7823ef295bb1ddc85c026ee8
Yatsuha <gobou1192noatti@gmail.com>
2024-06-26 21:04:44 +0900

    add readme

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# name
+
+image or gif
+
+## Overview
+
+## Requirement
+
+## Usage
+
+## Features
+
+## Reference
+
+## Author
+
+[twitter](https://twitter.com/Kotabrog)
+
+## Licence
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+        self.master.title("Acoustic")
+        self.master.geometry("400x400")
+    
