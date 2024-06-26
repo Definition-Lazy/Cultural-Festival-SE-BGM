59c5180cb7827291ed66f147883df4f687dac059
Yatsuha <gobou1192noatti@gmail.com>
2024-06-26 21:07:31 +0900

    add config

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
-        self.create_widgets()
         self.master.title("Acoustic")
         self.master.geometry("400x400")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
     
